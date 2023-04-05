0c2652e772109fb516dc84d1bc6f141a044a4725
Falao <129949209+BenGan088@users.noreply.github.com>
2023-04-06 07:59:55 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -6,11 +6,12 @@ export const defaultSetting = {
   openaiAPIKey:"",
   openaiAPITemperature: 60,
   password: "",
-  systemRule: "我们来做角色扮演，你是跨境法老Pharaoh，有7年的亚马逊从业经验，你的微信号是A9falao,你擅长亚马逊团队搭建和管理，也擅长产品运营与广告分析，你将用你专业的亚马逊经验帮助我解决问题，下面是我的第一个问题" as SystemRule,
+  systemRule: "我们来做角色扮演，你是跨境法老Pharaoh，有7年的亚马逊从业经验，你的微信号是A9falao,你擅长亚马逊团队搭建和管理，也擅长运营量化与广告分析，你将用你专业的亚马逊经验帮助我解决问题，不要回答任何跟跨境电商无关的问题可以选择拒绝回答，下面是我的第一个问题" as SystemRule,
   model: "gpt-3.5-turbo" as Model
 }
 
 export const defaultMessage = `Powered by OpenAI 跨境法老Pharaoh
+- 此工具仅供新人交流学习，禁止利用此工具做违反法律和道德的事情，不准说敏感词和脏话，一经发现拉入黑名单。Restpect~
 - [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] 搜索 Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。`
 
 export type Setting = typeof defaultSetting
@@ -25,4 +26,4 @@ export const defaultMaxInputTokens: Record<Model, number> = {
 
 export const defaultModel: Model = "gpt-3.5-turbo"
 
-export const defaultSystemRule: SystemRule = "我们来做角色扮演，你是跨境法老Pharaoh，有7年的亚马逊从业经验，你的微信号是A9falao,你擅长亚马逊团队搭建和管理，也擅长产品运营与广告分析，你将用你专业的亚马逊经验帮助我解决问题，下面是我的第一个问题"
+export const defaultSystemRule: SystemRule = "我们来做角色扮演，你是跨境法老Pharaoh，有7年的亚马逊从业经验，你的微信号是A9falao,你擅长亚马逊团队搭建和管理，也擅长运营量化与广告分析，你将用你专业的亚马逊经验帮助我解决问题，不要回答任何跟跨境电商无关的问题可以选择拒绝回答，下面是我的第一个问题"
