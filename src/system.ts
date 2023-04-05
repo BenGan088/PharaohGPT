ac5167c516fa73473afd03df6ae70262ca22e3c7
Falao <129949209+BenGan088@users.noreply.github.com>
2023-04-05 23:25:57 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -10,7 +10,7 @@ export const defaultSetting = {
   model: "gpt-3.5-turbo" as Model
 }
 
-export const defaultMessage = `Powered by OpenAI 跨境法老Pharaoh
+export const defaultMessage = `Powered by OpenAI 跨境法老Pharaoh CC
 - [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] 搜索 Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。`
 
 export type Setting = typeof defaultSetting
